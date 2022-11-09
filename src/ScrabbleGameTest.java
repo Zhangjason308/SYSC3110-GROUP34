04e64c6d88d7393ac035fa2dcf95cee91b400010
Jason <Zhangjason308@gmail.com>
2022-11-09 18:40:52 -0500

    Jason - added Scrabble Junit tests

---
+++ src/ScrabbleGameTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScrabbleGameTest {
+
+
+    @Test
+    void placeFirstWord() { //Makes sure first word has 1 piece in the center
+
+    }
+    @Test
+    void createHorizontalWord() { //A horizontal word is valid
+
+    }
+    @Test
+    void createVerticalWord() { //Vertical word is valid
+
+    }
+    @Test
+    void placeScatteredLetters() { //Make sure you can only place letters that are connected to each other linearly and at one starting point
+
+    }
+
+    @Test
+    void swapWorks() { //Allows a swap of letters in the hand
+
+    }
+
+    @Test
+    void swapDoesntWork() { //Swapped pieces you don't have in your hand
+
+    }
+
+    @Test
+    void placePieceThatMakesaHorizontalandVerticalWord() { //Place a piece that makes a horizontal and vertical word
+
+    }
+    @Test
+    void bagisOutofPieces()  { //Bag is out of pieces so game is over
+
+    }
+
+    @Test
+    void cantMakeAnyMoreWords()  { //Board can't formulate any more words
+
+    }
+
+
+}
