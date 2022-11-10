89290f809cf840a873d5c9da3b033e00d412073b
Jason <Zhangjason308@gmail.com>
2022-11-09 21:49:18 -0500

    Jason - added GameButtonFrame

--- src/ScrabbleFrame.java
+++ src/ScrabbleFrame.java
@@ -5,13 +5,14 @@ public class ScrabbleFrame extends JFrame implements ScrabbleView {
     JTextArea playerName = new JTextArea("Player 1"); //Make it have an actionlistener to chenge player everytime
     public ScrabbleFrame() {
         super("Scrabble");
-        BoardFrame board = new BoardFrame();
+        BoardFrame boardF = new BoardFrame();
+        // HandFrame handF = new HandFrame();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
         this.add(playerName, BorderLayout.NORTH);
-        //this.add(handFrame, BorderLayout.CENTER);
-        this.add(board, BorderLayout.CENTER);
-        this.add(board, BorderLayout.CENTER);
+        //this.add(handF, BorderLayout.SOUTH);
+        this.add(boardF, BorderLayout.CENTER);
+        this.add(boardF, BorderLayout.CENTER);
         this.setSize(800,800);
         this.setVisible(true);
 
