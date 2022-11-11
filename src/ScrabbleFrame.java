57f64b5fa86b5cd7b3ee7d172796cf30b5207841
Jason <Zhangjason308@gmail.com>
2022-11-11 02:05:56 -0500

    Revert "Jason - reverted commit"
    This reverts commit de41cea47c29f40e2ab2916fcf0a31da7e029d7c.

--- src/ScrabbleFrame.java
+++ src/ScrabbleFrame.java
@@ -2,46 +2,35 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ScrabbleFrame extends JFrame implements ScrabbleView {
-
+    JTextArea playerName = new JTextArea("Player 1"); //Make it have an actionlistener to chenge player everytime
     public ScrabbleFrame() {
         super("Scrabble");
-        ScrabbleGame model = new ScrabbleGame();
-        //Bag bag = new Bag();
-        //Hand hand = new Hand();
-        HandFrame handF = new HandFrame(model.player1Hand);
-        BoardFrame boardF = new BoardFrame(model.scrabbleBoard, handF);
+        Bag bag = new Bag();
+        Hand hand = new Hand();
+        hand.addPieces(bag.grabPieces(7));
+        HandFrame handF = new HandFrame(hand);
+        BoardFrame boardF = new BoardFrame(new Board(), handF);
         GameButtonFrame gameButtonF = new GameButtonFrame();
-        JTextArea gameName = new JTextArea("SCRABBLE"); //Make it have an actionlistener to change player everytime
-        //gameName.setAlignmentX();
-        InfoPanel infoP = new InfoPanel();
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout(20,20));
-        this.add(gameName, BorderLayout.NORTH);
+        this.add(playerName, BorderLayout.NORTH);
         this.add(handF, BorderLayout.SOUTH);
         this.add(gameButtonF, BorderLayout.LINE_START);
         this.add(boardF, BorderLayout.CENTER);
-        this.add(infoP, BorderLayout.LINE_END);
-        this.setSize(1000,800);
+        this.setSize(500,500);
         boardF.setSize(100,100);
         gameButtonF.setSize(100,100);
 
         this.setVisible(true);
 
     }
+    @Override
+    public void update(int x, int y, Piece selectedPiece) {
 
+
+    }
 
     public static void main(String args[]) {
         new ScrabbleFrame();
-    }
-
-    @Override
-    public void updateHandFrame(int x, int y, Piece selectedPiece) {
-
-    }
-
-    @Override
-    public void updateInfoPanel(int p1, int p2, Bag baggy) {
-
     }
 }
