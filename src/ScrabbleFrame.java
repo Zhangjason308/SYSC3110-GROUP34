21fca5059ffd3e65c3b3a07d05f175e543c77612
Jason <Zhangjason308@gmail.com>
2022-11-13 01:55:14 -0500

    Jason - fixed ScrabbleFrame

--- src/ScrabbleFrame.java
+++ src/ScrabbleFrame.java
@@ -1,32 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class ScrabbleFrame extends JFrame implements ScrabbleView {
+public class ScrabbleFrame extends JFrame {
 
     public ScrabbleFrame() {
         super("Scrabble");
         ScrabbleGame model = new ScrabbleGame();
-        //Bag bag = new Bag();
-        //Hand hand = new Hand();
-        HandFrame handF = new HandFrame(model.player1Hand);
-        BoardFrame boardF = new BoardFrame(model.scrabbleBoard, handF);
-        GameButtonFrame gameButtonF = new GameButtonFrame(model);
-        JTextArea gameName = new JTextArea("SCRABBLE"); //Make it have an actionlistener to change player everytime
-        //gameName.setAlignmentX();
-        InfoPanel infoP = new InfoPanel();
-
+        ScrabbleController sc = new ScrabbleController(model);
+        BoardPanel boardPanel = new BoardPanel(sc);
+        GameButtonPanel gameButtonPanel = new GameButtonPanel(sc);
+        InfoPanel infoPanel = new InfoPanel();
+        model.addScrabbleView(handPanel);
+        model.addScrabbleView(boardPanel);
+        model.addScrabbleView(infoPanel);
+        final JTextArea gameName = new JTextArea("SCRABBLE"); //Make it have an actionlistener to change player everytime
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout(20,20));
         this.add(gameName, BorderLayout.NORTH);
-        this.add(handF, BorderLayout.SOUTH);
-        this.add(gameButtonF, BorderLayout.LINE_START);
-        this.add(boardF, BorderLayout.CENTER);
-        this.add(infoP, BorderLayout.LINE_END);
+        this.add(handPanel, BorderLayout.SOUTH);
+        this.add(gameButtonPanel, BorderLayout.LINE_START);
+        this.add(boardPanel, BorderLayout.CENTER);
+        this.add(infoPanel, BorderLayout.LINE_END);
         this.setSize(1000,800);
-        boardF.setSize(100,100);
-        gameButtonF.setSize(100,100);
+        handPanel.setSize(800, 100);
+        boardPanel.setSize(700 ,700);
+        gameButtonPanel.setSize(100,100);
+        infoPanel.setSize(100, 800);
         this.setVisible(true);
 
+        model.updateViews();
     }
 
 
@@ -34,13 +36,5 @@ public class ScrabbleFrame extends JFrame implements ScrabbleView {
         new ScrabbleFrame();
     }
 
-    @Override
-    public void updateHandFrame(int x, int y, Piece selectedPiece) {
-
-    }
-
-    @Override
-    public void updateInfoPanel(int p1, int p2, Bag baggy) {
-
-    }
+    
 }
