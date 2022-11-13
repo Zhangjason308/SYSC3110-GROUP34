c1ffd81eac725e785c5bd7a22db48659d4311b33
Triguy12 <tritoncrowley@gmail.com>
2022-11-13 02:03:49 -0500

    Triotn - updated InfoPanel

--- src/InfoPanel.java
+++ src/InfoPanel.java
@@ -1,19 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
-public class InfoPanel extends JPanel{
+public class InfoPanel extends JPanel implements ScrabbleView{
     private JLabel tilesLeftLabel;
     private JLabel playerOne;
     private JLabel playerTwo;
+    private JLabel gameTurnLabel;
     private JLabel playerOneScore;
     private JLabel playerTwoScore;
 
     private int tilesLeft;
     private int p1ActualScore;
     private int p2ActualScore;
+    private boolean gameTurn;
 
     public InfoPanel(){
         super();
-        this.setSize(400, 500);
+        tilesLeft = 0;
+        p1ActualScore = 0;
+        p2ActualScore = 0;
+        gameTurn = true;
         this.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
@@ -38,6 +43,13 @@ public class InfoPanel extends JPanel{
         playerTwo = new JLabel("Player Two Score:");
         add(playerTwo, gbc);
 
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gameTurnLabel = new JLabel("turn: " + (gameTurn ? "Player 1" : "Player 2"));
+        add(gameTurnLabel, gbc);
+
         gbc.gridx = 2;
         gbc.gridy = 1;
         gbc.gridwidth = 1;
@@ -55,10 +67,17 @@ public class InfoPanel extends JPanel{
         setVisible(true);
 
     }
-    public void updateInfoPanel(Game.Player p1, Game.Player p2, Bag baggy) {
-        p1ActualScore = p1.score;
-        p2ActualScore = p2.score;
-        tilesLeft = baggy.numberOfRemainingPieces();
+    @Override
+    public void update(ScrabbleGame model) {
+        p1ActualScore = model.getPlayer1Score();
+        p2ActualScore = model.getPlayer2Score();
+        tilesLeft = model.getBag().numberOfRemainingPieces();
+        gameTurn = model.getTurn();
+
+        playerOneScore.setText(String.valueOf(p1ActualScore));
+        playerTwoScore.setText(String.valueOf(p2ActualScore));
+        tilesLeftLabel.setText(String.valueOf(tilesLeft));
+        gameTurnLabel.setText("turn: " + (gameTurn ? "Player 1" : "Player 2"));
     }
 
     public static void main(String[] args) {
