5c9a6b6d08fc4f03739fa0066df9629be26f64fb
Zhangjason308 <zhangjason308@gmail.com>
2022-11-30 12:11:58 -0500

    Jason - New MenuBarController

---
+++ src/MenuBarController.java
@@ -0,0 +1,18 @@
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+public class MenuBarController implements ActionListener {
+    private ScrabbleGame model;
+    public MenuBarController(ScrabbleGame model) {
+        this.model = model;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object o = e.getSource();
+        if (o instanceof JMenuItem) {
+
+        }
+    }
+}
