602a50ef11ce5686a27690909a058ab317aae75c
Jason <Zhangjason308@gmail.com>
2022-12-07 23:14:45 -0500

    Jason - Finished Start Frame View

--- src/StartFrame.java
+++ src/StartFrame.java
@@ -6,38 +6,72 @@ import java.io.File;
 import java.io.IOException;
 
 public class StartFrame extends JFrame {
+
     private JPanel playerSetting = new JPanel(new FlowLayout(FlowLayout.CENTER, 100, 20));
     private JPanel start = new JPanel(new BorderLayout(150,50));
 
-    private JPanel boardSetting = new JPanel(new FlowLayout(FlowLayout.CENTER, 100, 50));
+    private JPanel boardSetting = new JPanel(new FlowLayout(FlowLayout.CENTER, 100, 0));
+
+    private JPanel boardPanel = new JPanel(new BorderLayout(100,0));
     private JRadioButton player2 = new JRadioButton();
     private JRadioButton ai = new JRadioButton();
 
+    private JPanel boardOption = new JPanel(new FlowLayout(FlowLayout.CENTER, 100, 10));
+    private JRadioButton board1 = new JRadioButton();
+    private JRadioButton board2 = new JRadioButton();
+    private JRadioButton board3 = new JRadioButton();
     private JButton starts = new JButton("START");
 
     private ButtonGroup buttonGroup = new ButtonGroup();
+    private ButtonGroup boardGroup = new ButtonGroup();
+
+    final JTextField title = new JTextField("Welcome to Scrabble");
+    final JTextField playerOptions = new JTextField("Player Settings");
+    final JTextField boardSelection = new JTextField("Board Settings");
     public StartFrame(){
         super("Scrabble Game");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         BufferedImage boardImage1;
+        BufferedImage boardImage2;
+        BufferedImage boardImage3;
         try {
             boardImage1 = ImageIO.read(new File("src/Board1.jpg"));
+            boardImage2 = ImageIO.read(new File("src/Board2.jpg"));
+            boardImage3 = ImageIO.read(new File("src/Board3.jpg"));
             //Graphics2D
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        //boardImage1 = boardImage1.getScaledInstance(100,100,Image.SCALE_DEFAULT);
-        //Image scaledBoard1 = boardImage1.getScaledInstance(400,400,Image.SCALE_FAST);
-        JLabel picLabel = new JLabel(new ImageIcon(boardImage1));
-        picLabel.setSize(400,400);
-        this.add(picLabel,BorderLayout.CENTER);
+        JLabel picLabel1 = new JLabel(new ImageIcon(boardImage1));
+        JLabel picLabel2 = new JLabel(new ImageIcon(boardImage2));
+        JLabel picLabel3 = new JLabel(new ImageIcon(boardImage3));
+        //picLabel1.setSize(400,400);
+        //picLabel2.setSize(400,400);
+        boardPanel.add(boardSetting, BorderLayout.CENTER);
+        boardGroup.add(board1);
+        boardGroup.add(board2);
+        boardGroup.add(board3);
+        boardOption.add(boardSelection);
+        boardOption.add(board1);
+        boardOption.add(board2);
+        boardOption.add(board3);
+        boardPanel.add(boardOption, BorderLayout.SOUTH);
+        boardSetting.add(picLabel1);
+        boardSetting.add(picLabel2);
+        boardSetting.add(picLabel3);
         //ScrabbleFrame scrabbleFrame = new ScrabbleFrame();
-        this.setLayout(new BorderLayout(20,20));
-        final JTextField title = new JTextField("Welcome to Scrabble");
-        final JTextField playerOptions = new JTextField("Player Settings");
+        this.setLayout(new BorderLayout(20,120));
         player2.setText("Player 2");
         player2.setBounds(120, 30, 120, 50);
         ai.setBounds(250, 30, 80, 50);
         ai.setText("AI");
+        board1.setText("Board 1");
+        board2.setText("Board 2");
+        board3.setText("Board 3");
+        board1.setBounds(120, 80, 120, 10);
+        board2.setBounds(120, 80, 120, 10);
+        board3.setBounds(120, 80, 120, 10);
+
         buttonGroup.add(player2);
         buttonGroup.add(ai);
         playerSetting.add(playerOptions);
@@ -45,17 +79,22 @@ public class StartFrame extends JFrame {
         playerSetting.add(ai);
         player2.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 15));
         ai.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 15));
-        playerOptions.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 20));
-        playerOptions.setOpaque(false);
-        playerOptions.setBackground(Color.WHITE);
+        board1.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 15));
+        board2.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 15));
+        board3.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 15));
+        playerOptions.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 15));
         playerOptions.setEditable(false);
+        boardSelection.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 15));
+        boardSelection.setEditable(false);
+        boardSelection.setHorizontalAlignment(JTextField.CENTER);
+        title.setEditable(false);
         title.setFont(new Font(Font.SANS_SERIF,  Font.BOLD, 50));
         title.setHorizontalAlignment(JTextField.CENTER);
         title.setEditable(false);
         start.add(playerSetting,BorderLayout.CENTER);
         start.add(starts,BorderLayout.SOUTH);
-        this.add(picLabel, BorderLayout.NORTH);
-        //this.add(playerSetting, BorderLayout.SOUTH);
+        this.add(title, BorderLayout.NORTH);
+        this.add(boardPanel, BorderLayout.CENTER);
         this.add(start, BorderLayout.SOUTH);
         this.setVisible(true);
         this.setSize(1000,800);
